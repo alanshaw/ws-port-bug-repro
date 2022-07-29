fce8b87e7cf1673a0b9dd2acf3ce604cd055c318
Alan Shaw <alan.shaw@protocol.ai>
2022-07-29 11:21:28 +0100

    docs: add comment to explain ws-client

--- ws-client.js
+++ ws-client.js
@@ -1,6 +1,8 @@
 import WebSocket from 'ws'
 
-const ws = new WebSocket('ws://ws-port-bug-repro-1.loca.lt:80')
+// Expected to NOT work when connecting to 3000, but 80 is fine.
+// This is just proof that local tunnel does not accept requests on any port other than 80.
+const ws = new WebSocket('ws://ws-port-bug-repro-1.loca.lt:3000')
 
 ws.on('open', () => console.log('opened'))
 
